8751d0091ee96320eb7e67af75e62c910bf17dab
Evseev Ilja <ilja.evseev92@gmail.com>
2020-06-04 20:39:53 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# list_staff_members
+
+Тестовое задание EKF
+
+## Getting Started
+
---
+++ android/app/src/main/kotlin/com/example/list_staff_members/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.list_staff_members
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
